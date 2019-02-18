12ffd22188850a239a9d2194904f0e6a0e6b62bf
Luis Carlin <luiscarlin@gmail.com>
2019-02-18 05:25:38 -0500

    add post components

---
+++ src/components/postLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './layout'
+
+export default class postLayout extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Post Layout</h1>
+      </Layout>
+    )
+  }
+}
