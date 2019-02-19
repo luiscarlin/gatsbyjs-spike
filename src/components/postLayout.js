ce37ea7e3727bfb0fa60f62a63917c577bfa55c6
Luis Carlin <luiscarlin@gmail.com>
2019-02-19 05:48:24 -0500

    load post content

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from './layout'
 
 export default class postLayout extends React.Component {
   render() {
+    const { markdownRemark } = this.props.data
+
     return (
       <Layout>
-        <h1>Post Layout</h1>
+        <h1>{markdownRemark.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{
+          __html: markdownRemark.html
+        }}/>
       </Layout>
     )
   }
 }
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: {
+      slug: {
+        eq: $slug
+      }
+    }) {
+      html
+      frontmatter {
+        title
+        date
+        slug
+      }
+    }
+  }
+`
