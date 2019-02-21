250791806659515c5379434cd2f2b86d4a0368d8
Luis Carlin <luiscarlin@gmail.com>
2019-02-21 07:02:53 -0500

    add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    `gatsby-plugin-styled-components`,
     'gatsby-transformer-remark'
   ],
 }
